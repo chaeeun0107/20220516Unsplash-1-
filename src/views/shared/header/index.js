4b06ced731e529053800e732d6cae5a6a77c68b1
Jungchaeeun <123wjdcodms@gmail.com>
2022-05-19 01:05:03 +0900

    [GZ-5] Header 컴포넌트 검색영역 UI 개발

---
+++ src/views/shared/header/index.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+import { IconLogo } from '../../../icons';
+import SearchBox from '../searchBox';
+import Nav from './components/Nav';
+
+function Header() {
+  return (
+    <Container>
+      <IconLogo />
+      <SearchBox shape="round" />
+      <Nav />
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  padding: 0 20px;
+  display: flex;
+  //justify-content: space-between;
+  align-items: center;
+  font-size: 14px;
+  svg{
+    margin-right: 20px;
+  }
+`;
+
+export default Header;
